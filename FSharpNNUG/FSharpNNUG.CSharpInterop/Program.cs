b3ab59830d7cdd5ea459d2ca2e78a6f81cb7f505
Tormod <tormod.fjeldskaar@gmail.com>
2010-06-29 21:48:32 +0200

    Added C# project doing interop with F# library.

---
+++ FSharpNNUG/FSharpNNUG.CSharpInterop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FSharpNNUG.CSharpInterop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Fibonacci.nthFibonacci(10));
+			Console.ReadKey();
+		}
+	}
+}
